056d7eecce15e11ab18a7152d8277a69ec024bf4
Simon Garcia <core.excludesfile>
2021-01-05 14:31:31 -0500

    add setup.py for installation

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'nmreader',
+    version = 0.1,
+    description = 'Parse NMReady JCAMP-DX files for NMR data',
+    url = 'https://github.com/garcias/nmreadyreader',
+    author = 'Simon Garcia',
+    author_email = 'garcias@kenyon.edu',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['numpy', 'scipy.fftpack', 'altair', 'pandas', 'dataclasses', ],
+    python_requires='>=3.6',
+    keywords='nmr parsing jcamp dx',
+)
+
